a75485a9652515ac323690fb067cbf6bc501d16d
Daria Zhuromskaya <zhuromskaya_ds@mail.ru>
2022-02-20 00:25:38 +0700

    feat: validate

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,41 +1,28 @@
 const hasInvalidInput = (inputList) => {
-  // проходим по этому массиву методом some
   return inputList.some((inputElement) => {
-    // Если поле не валидно, колбэк вернёт true
-    // Обход массива прекратится и вся фунцкция
-    // hasInvalidInput вернёт true
-
     return !inputElement.validity.valid;
   })
 }; 
 
-const toggleButtonState = (inputList, buttonElement) => {
-  
-  // Если есть хотя бы один невалидный инпут
+const toggleButtonState = (inputList, buttonElement) => {  
   if (hasInvalidInput(inputList)) {
-    // сделай кнопку неактивной
     buttonElement.classList.add('popup__btn_inactive');
     buttonElement.disabled = true;
-  } else {
-    // иначе сделай кнопку активной
+  } else {    
     buttonElement.classList.remove('popup__btn_inactive');
     buttonElement.disabled = false;
   }
 };
 
 const showInputError = (formElement, inputElement, errorMessage) => {
-  // Находим элемент ошибки внутри самой функции
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  // Остальной код такой же
   inputElement.classList.add('popup__input_type_error');
   errorElement.textContent = errorMessage;
   errorElement.classList.add('popup__input-error_active');
 };
 
 const hideInputError = (formElement, inputElement) => {
-  // Находим элемент ошибки
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  // Остальной код такой же
   inputElement.classList.remove('popup__input_type_error');
   errorElement.classList.remove('popup__input-error_active');
   errorElement.textContent = '';
@@ -43,26 +30,20 @@ const hideInputError = (formElement, inputElement) => {
 
 const isValid = (formElement, inputElement) => {
   if (!inputElement.validity.valid) {
-    // showInputError теперь получает параметром форму, в которой
-    // находится проверяемое поле, и само это поле
     showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
-    // hideInputError теперь получает параметром форму, в которой
-    // находится проверяемое поле, и само это поле
     hideInputError(formElement, inputElement);
   }
 };
 
-const setEventListeners = (formElement) => {
-  
+const setEventListeners = (formElement) => {  
   const inputList = Array.from(formElement.querySelectorAll(`.popup__input`)); 
   const buttonElement = formElement.querySelector('.popup__btn');
   toggleButtonState(inputList, buttonElement);
-  
+
   inputList.forEach((inputElement) => {    
     inputElement.addEventListener('input', () => {
       isValid(formElement, inputElement);
-      // Вызовем toggleButtonState и передадим ей массив полей и кнопку
       toggleButtonState(inputList, buttonElement);
     });    
   });
